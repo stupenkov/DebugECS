1d6b07225597d85675e62bb6acc4e58317a2ccc0
Eleprog <stupenkov.anton@gmail.com>
2020-01-19 20:17:06 +0600

    Добавьте файлы проекта.

---
+++ ConnectionServer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ECS;
+
+namespace DebugECS
+{
+	public class ConnectionServer
+	{
+		private TcpClient _client;
+		private bool _connected = false;
+		private NetworkStream _networkStream;
+		private BinaryFormatter _binaryFormatter = new BinaryFormatter();
+
+		public event EventHandler<List<DataDebug>> DataReceived;
+
+		public int Port { get; } = 8888;
+
+		public string Server { get; } = "127.0.0.1";
+
+		public void Connect()
+		{
+			Task.Run(() =>
+			{
+				while (true)
+				{
+					if (_connected == false)
+					{
+						TryConnect();
+					}
+
+					Thread.Sleep(1000);
+				}
+			});
+		}
+
+		private void TryConnect()
+		{
+			try
+			{
+				Console.WriteLine("Попытка подключения.");
+				_client = new TcpClient();
+				_client.Connect(Server, Port);
+				_connected = true;
+				_networkStream = _client.GetStream();
+
+				while (_connected)
+				{
+					ReceiveData();
+				};
+			}
+			catch (SocketException e)
+			{
+				Console.WriteLine($"Не удалось подключиться к серверу {Server}:{Port}");
+			}
+		}
+
+		private void ReceiveData()
+		{
+			try
+			{
+				List<DataDebug> dataDebugs =(List<DataDebug>)_binaryFormatter.Deserialize(_networkStream);
+				DataReceived?.Invoke(this, dataDebugs);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Соединение разорвано.");
+				_connected = false;
+				_networkStream.Close();
+				_client.Close();
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using ECS;
+using ECS.Drawing;
+
+namespace DebugECS
+{
+	class Program
+	{
+		private const int port = 8888;
+		private const string server = "127.0.0.1";
+
+		static void Main(string[] args)
+		{
+			ConnectionServer connection = new ConnectionServer();
+			connection.DataReceived += Connection_DataReceived;
+			connection.Connect();
+			while (true)
+			{
+			}
+		}
+
+		private static void Connection_DataReceived(object sender, List<DataDebug> e)
+		{
+			Console.Clear();
+			foreach (var item in e)
+			{
+				Console.WriteLine(item.Entity);
+				foreach (var i in item.EntityComponents)
+				{
+					Console.WriteLine("\t" + i.Key + "  ");
+					foreach (var v in i.Value)
+					{
+						Console.WriteLine("\t\t" + v + " ");
+					}
+				}
+			}
+		}
+	}
+}
